e7b4b3c426a6d12eb5ac5a20f6b04bf20607231d
JojoNeedsPizza <johannes.faizade@gmx.de>
2026-06-15 16:56:03 +0200

    Update app.py
    Workssss xD

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 print("Better AI Chat Than ChatGPT")
 print("Bozo.ai")
 print("woowowowowowoow")
-
 # Wont share my Ai key with yall xD
-
 apikey = input("Input your Google AI Studio API Key:")
 
 from google import genai
@@ -11,7 +9,7 @@ from google import genai
 client = genai.Client(api_key= apikey)
 
 user_inputanswer= input("Gimme ur question Bozo ^^:")
-
+uia = user_inputanswer
 response = client.models.generate_content(
     model="gemini-3.5-flash",
     contents=user_inputanswer
@@ -19,7 +17,11 @@ response = client.models.generate_content(
 print(response.text)
 
 
+if uia =="+Exit+":
+  repeat = False
 
-while repeat is True:
-
- print("Success")
+else:
+  repeat = True
+  while repeat is True:
+   print("Success")
+   break
