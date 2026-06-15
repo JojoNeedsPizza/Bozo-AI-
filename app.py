2598686c99e3771dc7b8306a9cf2149c08969888
JojoNeedsPizza <johannes.faizade@gmx.de>
2026-06-15 16:49:12 +0200

    Create app.py
    Basic Structure, testing out Stuff and removing my API Key and replacing it with an input and im testing the while thing (still new in python)

---
+++ app.py
@@ -0,0 +1,25 @@
+print("Better AI Chat Than ChatGPT")
+print("Bozo.ai")
+print("woowowowowowoow")
+
+# Wont share my Ai key with yall xD
+
+apikey = input("Input your Google AI Studio API Key:")
+
+from google import genai
+
+client = genai.Client(api_key= apikey)
+
+user_inputanswer= input("Gimme ur question Bozo ^^:")
+
+response = client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents=user_inputanswer
+)
+print(response.text)
+
+
+
+while repeat is True:
+
+ print("Success")
